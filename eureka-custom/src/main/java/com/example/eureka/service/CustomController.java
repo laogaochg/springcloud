8fde8e1944c824d68f7410acc406a566e77fdbe0
laogaozhg <laogaozhg@qq.com>
2017-12-13 19:35:30 +0800

    实现调用之间泛型传递

--- eureka-custom/src/main/java/com/example/eureka/service/CustomController.java
+++ eureka-custom/src/main/java/com/example/eureka/service/CustomController.java
@@ -1,24 +1,43 @@
 package com.example.eureka.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@Controller
+import com.example.eureka.dto.ExecuteResult;
+import com.example.eureka.util.RestTemplateUtil;
+
+@RestController
 public class CustomController {
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping(value = "/find")
-    @ResponseBody
     public String find() {
         //url中对应api提供者的名称，全大写
         User u = new User("111","天天");
         String s = restTemplate.postForEntity("http://CLOUD-SIMPLE-SERVICE/getUser",u,String.class).getBody();
         System.out.println("s = " + s);
         return s;
+    }
+    @RequestMapping("getOrderInnDayList")
+    public List<User> getOrderInnDayList(String orderCode,Integer memberId){
+        Map<String, Object> params = new HashMap<>();
+        params.put("orderCode", orderCode);
+        params.put("memberId", memberId);
+        String url = "http://CLOUD-SIMPLE-SERVICE/getUserExecute";
+        //use postForList
+        ExecuteResult<List<User>> result = RestTemplateUtil.postForList(url, params,
+                new ParameterizedTypeReference<ExecuteResult<List<User>>>(){});
+        return result.getData();
     }
 
     /**
@@ -32,4 +51,6 @@ public class CustomController {
         //url中对应api提供者的名称，全大写
         return restTemplate.getForEntity("http://www.baidu.com/",String.class).getBody();
     }
+
+
 }
--- eureka-prodive/src/main/java/com/example/prodive/controller/ProvideServiceController.java
+++ eureka-prodive/src/main/java/com/example/prodive/controller/ProvideServiceController.java
@@ -1,18 +1,41 @@
 package com.example.prodive.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.prodive.dto.ExecuteResult;
 
 @RestController
 public class ProvideServiceController {
 
     @RequestMapping("/getUser")
-    public User doDiscoveryService(@RequestBody User us) {
+    public User getUser(@RequestBody User us) {
         System.out.println("us = " + us);
         User u = new User();
         u.setId("111");
         u.setName("天天");
         return u;
+    }
+
+    @RequestMapping("/getUserExecute")
+    public ExecuteResult<List<User>> getUserExecute(@RequestBody User us) {
+        System.out.println("us = " + us);
+        ExecuteResult<List<User>> result = new ExecuteResult<>();
+        List<User> uss = new ArrayList<>();
+        User u = new User();
+        u.setId("111");
+        u.setName("天天");
+        uss.add(u);
+        User u2 = new User();
+        u2.setId("2222");
+        u2.setName("aaaaa");
+        uss.add(u2);
+        result.setData(uss);
+        result.setSuccess(true);
+        return result;
     }
 }
