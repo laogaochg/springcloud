b128e06cfec9e90a903ee47ccd18bf3fa4de69ec
laogaozhg <laogaozhg@qq.com>
2018-01-27 00:29:50 +0800

    声明式调用服务

--- eureka-prodive/src/main/java/com/example/prodive/controller/ProvideServiceController.java
+++ eureka-prodive/src/main/java/com/example/prodive/controller/ProvideServiceController.java
@@ -1,6 +1,9 @@
 package com.example.prodive.controller;
+
 import com.example.eureka.dto.ExecuteResult;
+import com.example.eureka.dto.User;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,36 @@ public class ProvideServiceController {
         u.setName("天天");
         return u;
     }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        User u = new User();
+        u.setId("111");
+        u.setName("天天");
+        return u.toString();
+    }
+
+    @RequestMapping("/hello2")
+    public User hello2(@RequestHeader String name,String age) {
+        System.out.println("ProvideServiceController.hello2");
+        User u = new User();
+        u.setId(name);
+        u.setName(age);
+        return u;
+    }
+
+    @RequestMapping("/hello3")
+    public String hello3(@RequestBody User u) {
+        System.out.println("ProvideServiceController.hello3");
+        return "hello3:" + u.toString();
+    }
+    @RequestMapping("/hello4")
+    public List<User> hello4(@RequestBody List<User> u) {
+        System.out.println("ProvideServiceController.hello3"+u);
+        u.addAll(u);
+        return u;
+    }
+
 
     @RequestMapping("/getUserExecute")
     public ExecuteResult<List<User>> getUserExecute(@RequestBody User us) {
